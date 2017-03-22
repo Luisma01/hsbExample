88d845f204c3fffc5b0801ba0980c62bc53c3fae
Luis Marcelo <luis4471@hotmail.com>
2017-03-22 16:46:12 -0500

    hello

---
+++ Desktop/MARCELO/Aplicacion_Comentarios/Aplicacion_Comentarios/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Aplicacion_Comentarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            string m_sArchive = @"C:\Users\hsbUser\Desktop\MARCELO/hola.txt";
+
+            using(FileStream Fs = File.OpenRead(m_sArchive))
+            {
+
+                byte[] b = new byte[2000];
+                UTF8Encoding Un = new UTF8Encoding(true);
+                
+                while (Fs.Read(b,0,b.Length) > 0) 
+                {
+
+
+                Console.WriteLine(Un.GetString(b));
+                
+                }
+
+                Console.ReadKey();
+
+            }
+
+
+
+
+        }
+    }
+}
